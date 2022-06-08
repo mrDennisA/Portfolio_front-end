45eb04adeec2576399ee2783e3c0f53d7062f67d
mrDennisA <Dennis.Alekseev@gmail.com>
2022-06-08 09:07:00 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,47 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
+// import { useRouter } from "next/router";
 
 // Components
 import Layout from "../Layout";
-import LoadingTransition from "../components/LoadingTransition";
+// import LoadingTransition from "../components/LoadingTransition";
 
 // Styles
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
+  // const [loading, setLoading] = useState(false);
+  // const router = useRouter();
 
-  useEffect(() => {
-    const handleStart = (url) => {
-      if (router.route !== url) {
-        setLoading(true);
-      }
+  // useEffect(() => {
+  //   const handleStart = (url) => {
+  //     if (router.route !== url) {
+  //       setLoading(true);
+  //     }
 
-      // console.log(`Loading: ${url}`);
-      // console.log(router.route !== url);
-    };
-    const handleStop = (url) => {
-      setLoading(false);
-      // console.log(`stop: ${url}`);
-    };
+  //     // console.log(`Loading: ${url}`);
+  //     // console.log(router.route !== url);
+  //   };
+  //   const handleStop = (url) => {
+  //     setLoading(false);
+  //     // console.log(`stop: ${url}`);
+  //   };
 
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleStop);
-    router.events.on("routeChangeError", handleStop);
+  //   router.events.on("routeChangeStart", handleStart);
+  //   router.events.on("routeChangeComplete", handleStop);
+  //   router.events.on("routeChangeError", handleStop);
 
-    return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleStop);
-      router.events.off("routeChangeError", handleStop);
-    };
-  }, [router]);
+  //   return () => {
+  //     router.events.off("routeChangeStart", handleStart);
+  //     router.events.off("routeChangeComplete", handleStop);
+  //     router.events.off("routeChangeError", handleStop);
+  //   };
+  // }, [router]);
 
   return (
     <Layout>
-      <LoadingTransition loading={loading}>
-        <Component {...pageProps} />
-      </LoadingTransition>
+      {/* <LoadingTransition loading={loading}> */}
+      <Component {...pageProps} />
+      {/* </LoadingTransition> */}
     </Layout>
   );
 }
